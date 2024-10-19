c74da390f478fc9456d2e5959940d2f5cf862929
irshad017 <irshadhussain7881@gmail.com>
2024-10-19 16:10:08 +0530

    UI

--- frontend/components/dashboard.tsx
+++ frontend/components/dashboard.tsx
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Chart, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
 import socket from "@/utils/socket";
 import { Line } from "react-chartjs-2";
+import StatsCard from "./StatsCard";
+import Sidebar from "./SideBar";
+import BarChart from "./BarChart";
+import PieChartCard from "./PieChartCard";
+import LineChart from "./LineChart";
+import StackedBarChart from "./StackedBarChart";
 
 Chart.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
@@ -42,6 +48,33 @@ const Dashboard: React.FC = () => {
         <div>
             <h2>Real-time Data Chart</h2>
             <Line data={chartData} options={{ responsive: true }} />
+            <div className="min-h-screen flex">
+            
+            <Sidebar />
+
+            
+            <main className="flex-1 p-6 bg-gray-100">
+                
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                <StatsCard title="Statistics 1" value="21%" />
+                <StatsCard title="Statistics 2" value="56%" />
+                <StatsCard title="Statistics 3" value="75%" />
+                <StatsCard title="Statistics 4" value="100%" />
+                </div>
+
+                
+                <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6">
+                <BarChart title="Sales Statistics" />
+                <PieChartCard title="Customer Demographics" />
+                </div>
+
+                
+                <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6">
+                <LineChart title="Growth Statistics" />
+                <StackedBarChart title="Product Sales" />
+                </div>
+            </main>
+</div> 
         </div>
     );
 };
