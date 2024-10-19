407271c373c6e7d675f565471b21a71cd237a82f
irshad017 <irshadhussain7881@gmail.com>
2024-10-19 20:27:20 +0530

    toast

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Dashboard from "@/components/dashboard";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; 
 
 const Home: React.FC = () => {
   return (
     <div className="container">
       <Dashboard />
+      <ToastContainer /> 
     </div>
   );
 };
--- frontend/utils/socket.ts
+++ frontend/utils/socket.ts
@@ -1,21 +1,26 @@
-const URL = process.env.NEXT_PUBLIC_BACKEND_URL
+import { toast } from "react-toastify";
+
+const URL = process.env.NEXT_PUBLIC_BACKEND_URL; 
 const socket = new WebSocket(`${URL}/ws`);
 
-socket.addEventListener('open', () => {
+socket.addEventListener("open", () => {
     console.log("Connected to WebSocket server");
+    toast.success("Connected to WebSocket server");
 });
 
-socket.addEventListener('message', (event) => {
+socket.addEventListener("message", (event) => {
     const message = JSON.parse(event.data);
     console.log("Message from server:", message);
 });
 
-socket.addEventListener('error', (error) => {
+socket.addEventListener("error", (error) => {
     console.error("WebSocket connection error:", error);
+    toast.error(`WebSocket connection error: ${error}`);
 });
 
-socket.addEventListener('close', () => {
-    console.log("WebSocket connection closed.");
+socket.addEventListener("close", (event) => {
+    console.log("WebSocket connection closed:", event);
+    toast.info("WebSocket connection closed.");
 });
 
 export default socket;
